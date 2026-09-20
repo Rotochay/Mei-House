1ab1f0ee8da6903a08f736eeca3b7bb12834d88d
Rotochay <clairepeats@outlook.com>
2026-09-20 08:22:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const year = document.getElementById("year");
+
+if (year) {
+    year.textContent = new Date().getFullYear();
+}
+
+
+/* Smooth scrolling */
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", event => {
+
+        const target = document.querySelector(
+            link.getAttribute("href")
+        );
+
+        if (!target) return;
+
+        event.preventDefault();
+
+        target.scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+
+    });
+
+});
